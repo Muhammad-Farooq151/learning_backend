7170da1855c1b46f5020116b7d9d51f71c03b12f
Your Name <miangali927@gmail.com>
2026-01-27 03:12:41 +0500

    payments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllUsers } = require('../controllers/userController');
+const { getAllUsers, getProfile, updateProfile } = require('../controllers/userController');
 
 // GET /api/users
 router.get('/', getAllUsers);
+
+// POST /api/users/profile - Get user profile
+router.post('/profile', getProfile);
+
+// PUT /api/users/profile - Update user profile
+router.put('/profile', updateProfile);
 
 module.exports = router;
 
