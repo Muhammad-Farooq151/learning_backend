344411457e25f198ec69dd9eddf22de63ea45937
Your Name <miangali927@gmail.com>
2026-02-05 04:00:22 +0500

    admin restirction or admin role

--- routes/tutorRoutes.js
+++ routes/tutorRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { adminAuth } = require('../middleware/adminMiddleware');
 const {
   createTutor,
   getAllTutors,
@@ -7,19 +8,20 @@ const {
   updateTutor,
   deleteTutor,
 } = require('../controllers/tutorController');
-// Create new tutor
-router.post('/', createTutor);
 
-// Get all tutors
+// Create new tutor - Admin only
+router.post('/', adminAuth, createTutor);
+
+// Get all tutors - Public
 router.get('/', getAllTutors);
 
-// Get single tutor by ID
+// Get single tutor by ID - Public
 router.get('/:id', getTutorById);
 
-// Update tutor
-router.put('/:id', updateTutor);
+// Update tutor - Admin only
+router.put('/:id', adminAuth, updateTutor);
 
-// Delete tutor
-router.delete('/:id', deleteTutor);
+// Delete tutor - Admin only
+router.delete('/:id', adminAuth, deleteTutor);
 
 module.exports = router;
